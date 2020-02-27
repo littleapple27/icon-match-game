8c155aab1d7c3db529c7b7182c8f056ccfa9a369
littleapple27 <38861849+littleapple27@users.noreply.github.com>
2020-02-27 11:55:44 -0500

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -1,10 +1,11 @@
+/* Method to show demographic form on page load */ 
+     $(window).on('load',function(){
+        //$('#myModal').modal('show');
+        loadGame();
+    });
 
 	/* This is the array that contains the icon deck (32 icons) */
 	var deck=[
-		{  
-         name:"fa-video",
-         target:"video"
-      	},
       { 
          name:"fa-bolt",
          target:"flash"
@@ -23,7 +24,7 @@
       },
       { 
          name:"fa-cloud-upload-alt",
-         target:"cloud<br />upload"
+         target:"cloud upload"
       },
       { 
          name:"fa-compress",
@@ -31,7 +32,7 @@
       },
       { 
          name:"fa-concierge-bell",
-         target:"request<br />service"
+         target:"request service"
       },
       { 
          name:"fa-credit-card",
@@ -55,7 +56,7 @@
       },
       { 
          name:"fa-external-link-alt",
-         target:"external<br />link"
+         target:"external link"
       },
       { 
          name:"fa-eye",
@@ -63,7 +64,7 @@
       },
       { 
          name:"fa-grip-lines",
-         target:"grip<br />lines"
+         target:"grip lines"
       },
       { 
          name:"fa-life-ring",
@@ -71,7 +72,7 @@
       },
       { 
          name:"fa-location-arrow",
-         target:"location<br />arrow"
+         target:"location arrow"
       },
       { 
          name:"fa-lock",
@@ -79,7 +80,7 @@
       },
       { 
          name:"fa-moon",
-         target:"sleep<br />mode"
+         target:"sleep mode"
       },
       { 
          name:"fa-street-view",
@@ -87,7 +88,7 @@
       },
       { 
          name:"fa-th",
-         target:"table<br />view"
+         target:"table view"
       },
       { 
          name:"fa-address-book",
@@ -99,7 +100,7 @@
       },
       { 
          name:"fa-bus",
-         target:"bus<br />route"
+         target:"bus route"
       },
       { 
          name:"fa-calendar-alt",
@@ -123,7 +124,7 @@
       },
       { 
          name:"fa-power-off",
-         target:"power<br />off"
+         target:"power"
       },
       { 
          name:"fa-trash-alt",
@@ -155,7 +156,7 @@
       },
       { 
          name:"fa-download",
-         target:"file<br />download"
+         target:"file download"
       },
       { 
          name:"fa-ellipsis-h",
@@ -163,7 +164,7 @@
       },
       { 
          name:"fa-globe",
-         target:"language<br />settings"
+         target:"language settings"
       },
       { 
          name:"fa-heart",
@@ -175,7 +176,7 @@
       },
       { 
          name:"fa-map-marker-alt",
-         target:"location<br />marker"
+         target:"location marker"
       },
       { 
          name:"fa-pencil-alt",
@@ -191,7 +192,7 @@
       },
       { 
          name:"fa-shopping-cart",
-         target:"shopping<br />cart"
+         target:"shopping cart"
       },
       { 
          name:"fa-star",
@@ -209,34 +210,29 @@
 	/* The arrays that contain the cards that have been matched so far */
 	var deckMatched=[];
 	/* Variables that contain the value of the card picked from each deck /*/
-	var deck1flipped='';
-	var deck2flipped='';
+	var deck1select='';
+	var deck2select='';
 	/* Variables that identify the 2 divs for the decks */
 	var deck1div=document.getElementById('deck1');
 	var deck2div=document.getElementById('deck2');
-	
-     
-   $(document).ready(function(){
-        $("#myModal").modal('show');
-    }); 
+   
 
-	/*
-		Initialize a new game
-	*/
+
+	
+
+/* Function to initialize a new game */
 	function loadGame(){
-		/* Get the difficulty of the game */
-		//var element=document.getElementById('difficulty');  REMOVE
-		//var value=element.options[element.selectedIndex].value; REMOVE
-		//console.log(value);
-		/* Set value to 20 cards */
-		var value = 18; 
+		/* Set value to 18 cards */
+		var value = 18;
+      console.log(value);
+      
 		/* Initialize the board*/
 		initializeBoard();
 		/* Shuffle the deck with 52 cards */
 		deck=shuffle(deck);
 		/* Populate the 2 decks with X cards based on 20 cards level */
 		for(i=0;i<value;i++){
-			deck1.push(deck[i].name) 
+			deck1.push(deck[i].target) 
 			deck2.push(deck[i].target);
 		}
 		console.log(deck1);
@@ -246,9 +242,9 @@
 		/* Add the cards to the webpage */
 		for(i=0;i<value;i++){
 			/* Create the image elements*/
-			card1='<div class="col-md-4 col-6 p-2"><div class="card card-flip border-0"><div class="card-front text-white bg-purple"><div class=" noselect card-body p-0 d-flex align-items-center justify-content-center"><i class="fa fa-question fa-1x float-middle"></i></div></div><div class="card-back bg-white"><div class=" noselect card-body p-0 d-flex align-items-center justify-content-center" id="back-icon-size"><i class="fas ' + deck1[i]+'  float-middle"></i></div></div></div></div>';
-			card2='<div class="col-md-4 col-6 p-2"><div class="card card-flip border-0"><div class="card-front text-white bg-teal"><div class=" noselect card-body p-0 d-flex align-items-center justify-content-center"><i class="fa fa-question fa-1x float-middle"></i></div></div><div class="card-back bg-white"><div class=" noselect card-body  p-0 d-flex align-items-center justify-content-center" id="back-text-size">' +deck2[i] +'</div></div></div></div>';
-			/*Add images to the 2 decks */
+			card1='<div class="col-md-4 col-6 p-1 d-flex justify-content-center"><div class="card card-flip border-0 shadow"><div class="card-front text-white bg-lt-teal"><div class=" noselect card-body p-0 d-flex align-items-center justify-content-center"><i class="fa fa-question fa-1x float-middle"></i></div></div><div class="noselect card-back p-0 d-flex align-items-center justify-content-center" id="back-icon" data-target="' +deck1[i] +'"><i class="fas ' + deck[i].name +'  float-middle" data-target="' +deck1[i] +'"></i></div></div></div></div>';
+			card2='<div class="col-md-4 col-6 p-1 d-flex justify-content-center"><div class="card card-flip border-0 shadow"><div class="card-front text-white bg-lt-teal"><div class=" noselect card-body p-0 d-flex align-items-center justify-content-center"><i class="fa fa-question fa-1x float-middle"></i></div></div><div class="noselect card-back p-0 d-flex align-items-center justify-content-center" id="back-text" data-target="' +deck2[i] +'">' +deck2[i] +'</div></div></div>';
+			/*Add information to the 2 decks */
 			deck1div.innerHTML+=card1;
 			deck2div.innerHTML+=card2;
 		}
@@ -264,117 +260,174 @@
 		deck2=[];
 		/* Clear the cards that have been matched */
 		deckMatched=[];
-		/* Clear the variables that contain the cards that have been flipped */
-		deck1flipped='';
-		deck2flipped='';
+		/* Clear the variables that contain the cards that have been selected */
+      deck1select='';
+      deck2select='';
 	}
 
-	/* When the user clicks on start game the cards flip over and the timer starts?? */
+	/* When the user clicks on start game the cards flip over and the timer starts */
 	function startGame(){
+      startTimer();
   		$('.card-front').addClass('card-front-flip').removeClass('.card-front');
 		$('.card-back').addClass('card-back-flip').removeClass('.card-back');
-		$('#icon-bank').addClass('bg-purple').removeClass('.bg-lt-purple');
-		$('#word-bank').addClass('bg-teal').removeClass('.bg-lt-teal');
+      $('#start-btn').html('<i class="fa fa-repeat"></i>&nbsp;&nbsp;Replay');
+		//$('#icon-bank').addClass('bg-teal').removeClass('.bg-lt-teal');
+		//$('#word-bank').addClass('bg-lt-teal').removeClass('.bg-teal');
 		//$('#startBtn').html("Start Over");
 	};
 
-	
+
+
 	/* When the user clicks on deck1 */
-	deck1div.addEventListener('click',function(event){
-		/* If the user clicks outside a card, don't do anything */
-		if(event.target.getAttribute('data-card')==undefined) return;
-		/* Check if the card has already been matched */
-		if(deckMatched.includes(event.target.getAttribute('data-card'))) return;
-		/* get the value of the card and set it as flipped */
-		deck1flipped=event.target.getAttribute('data-card');
-		console.log("deck1flipped - "+deck1flipped);
-		console.log("deck2flipped - "+deck2flipped);
-		/*Flip all the cards */
-		flipUnmatchedCards('deck1','red_back');
-		/* Flip the selected card */
-		event.target.src='img/'+event.target.getAttribute('data-card')+'.png';
-		if(deck2flipped.length>0){
-			checkMatch(event.target.getAttribute('data-card'));
+      deck1div.addEventListener('click',function(event){  
+      /* If the user clicks outside a card, don't do anything */
+         if(event.target.getAttribute('data-target')==undefined) return; 
+      /* Check if the card has already been matched */
+	      if(deckMatched.includes(event.target.getAttribute('data-card'))) return; 
+      
+      /* get the value of the card and set it as selected */
+	         deck1select=event.target.getAttribute('data-target')    
+            console.log("deck1select - "+deck1select);
+	         console.log("deck2select - "+deck2select);
+      
+      //INSERT CHANGE OF BG COLOR FUNCTION HERE i.e.
+     event.target.style.backgroundColor = "#eee";
+   
+     
+      if(deck2select.length>0){
+			checkMatch(event.target.getAttribute('data-target'));
 		}
-		console.log(event.target.getAttribute('data-card'));
-	});
+		console.log(event.target.getAttribute('data-target'));
 	
-	/* When the user clicks on deck2 */
-	deck2div.addEventListener('click',function(event){
-		/* If the user clicks outside a card, don't do anything */
-		if(event.target.getAttribute('data-card')==undefined) return;
-		/* Check if the card has already been matched */
-		if(deckMatched.includes(event.target.getAttribute('data-card'))) return;
-		/* get the value of the card and set it as flipped */
-		deck2flipped=event.target.getAttribute('data-card');
-		console.log("deck1flipped - "+deck1flipped);
-		console.log("deck2flipped - "+deck2flipped);
-		/*Flip all the cards */
-		flipUnmatchedCards('deck2','yellow_back');
-		/* Flip the selected card */
-		event.target.src='img/'+event.target.getAttribute('data-card')+'.png';
-		if(deck1flipped.length>0){
-			checkMatch(event.target.getAttribute('data-card'));
+   }); ///end of when user clicks on deck1   
+         
+
+	
+	   /* When the user clicks on deck2 */
+	   deck2div.addEventListener('click',function(event){  
+         console.log('deck2 clicked');
+
+      /* If the user clicks outside a card, don't do anything */
+      if(event.target.getAttribute('data-target')==undefined) return;
+
+      /* Check if the card has already been matched */
+      if(deckMatched.includes(event.target.getAttribute('data-target'))) return;
+
+      /* get the value of the card and set it as selected */
+	   deck2select=event.target.getAttribute('data-target');
+	   console.log("deck1select - "+deck1select);
+	   console.log("deck2select - "+deck2select);
+
+     
+
+      if(deck1select.length>0){
+			checkMatch(event.target.getAttribute('data-target'));
 		}
-		console.log(event.target.getAttribute('data-card'));
-	});
-	
-	
+		console.log(event.target.getAttribute('data-target'));
+   });
+
+
 	function checkMatch(cardValue){
-		/* Clear the flipped cards */
-		deck1flipped='';
-		deck2flipped='';
-		if(deck2flipped==deck1flipped){
+	/* Clear the selected cards */
+	if(deck2select===deck1select){
+      console.log("winner winner chicken dinner")
 			/* We have a match*/
 			/* Add the cards to the matched array */
 			deckMatched.push(cardValue);
-			console.log(deckMatched);
-			/* Clear the flipped cards */
-			checkEndgame();
-		}else{
-			console.log('deck2div - not matched');
-			/* Flip the cards after 1500 milliseconds */
-			setTimeout(function(){
-				/* Flip the card that haven't been matched */
-				flipUnmatchedCards('deck1',back='red_back');
-				flipUnmatchedCards('deck2',back='yellow_back');
-			},1000);
-		}
-	}
+	      console.log(deckMatched);
+	      /* Clear the select cards */
+	 		checkEndgame();
+		} else {
+	console.log('deck2div - not matched');
+	/* Flip the cards after 1500 milliseconds */
+	// 		// setTimeout(function(){
+	// 		// 	/* Flip the card that haven't been matched */
+	// 		// 	flipUnmatchedCards('deck1',back='red_back');
+	// 		// 	flipUnmatchedCards('deck2',back='yellow_back');
+	// 		// },1000);
+	// 	}
+	 }
+   }
 
 	/* Checks if the user won the game */
-	function checkEndgame(){
-		setTimeout(function(){
-				/*Display congratulations message */
-				if(deck1.length==deckMatched.length) alert('Congratulations! Time to play a more difficult level!');
-			},2000);
+	   function checkEndgame(){
+	   setTimeout(function(){
+	/*Display congratulations message */
+	if(deck1.length==deckMatched.length) alert('Congratulations! You\'ve matched all of the icons!');
+	},2000);
 	}
+
 	
 	/* Flip all the cards that have not been matched yet */
-	function flipUnmatchedCards(deck,back=''){
+	function matchedCards(deck){
 		/* Select one of the two decks */
 		var deck=document.getElementById(deck);
 		/* Select all the cards in the deck */
-		var cards = deck.getElementsByClassName('card');
+		var cards = deck.getElementsByClassName('card-back');
 		var currentCard;
-		/* Flip all the cards */
+		/* Color all the cards */
 		for(i=0;i<cards.length;i++){
 			/* Get value of the card */
-			currentCard=cards[i].getAttribute('data-card');
-			/* Check if the card has been matched so that we don't flip it */
-			if(!deckMatched.includes(currentCard)) cards[i].src='img/'+back+'.png';
-		}
+         currentCard=cards[i].getAttribute('data-card');
+			if(!deckMatched.includes(currentCard)) 
+          /* Check if the card has been matched so that we don't color it */
+			currentCard=cards[i].style.backgroundColor = "#f2f2f2";
+      }
 	}
 
-		/****** Exit Message ******/
-		$(window).bind('beforeunload', function(){
-			return 'Are you sure you want to leave?';
-		});
+
+    /* Function to start timer */
+    var clearTime; 
+    var seconds = 0, minutes = 0, hours = 0;
+    var clearState; 
+    var secs, mins, gethours ; 
+    //Timer start function  
+    function startTimer() {
+    /* check if seconds is equal to 60 and add a +1 to minutes, and set seconds to 0 */ 
+    if ( seconds === 60 ) { seconds = 0; minutes = minutes + 1; }
+    /* you use the javascript tenary operator to format how the minutes should look and add 0 to minutes if less than 10 */ 
+    mins = ( minutes < 10 ) ? ( '0' + minutes + ': ' ) : ( minutes + ': ' );
+    /* check if minutes is equal to 60 and add a +1 to hours set minutes to 0 */ 
+    if ( minutes === 60 ) { minutes = 0; hours = hours + 1; }
+    /* you use the javascript tenary operator to format how the hours should look and add 0 to hours if less than 10 */
+    gethours = ( hours < 10 ) ? ( '0' + hours + ': ' ) : ( hours + ': ' );
+    secs = ( seconds < 10 ) ? ( '0' + seconds ) : ( seconds ); 
+    // display the stopwatch 
+    var time =gethours + mins + secs;
+    $('.score-section').find('.timer').html('<i class="fas fa-stopwatch"></i>&nbsp'+time);
+    /* call the seconds counter after displaying the stop watch and increment seconds by +1 to keep it counting */
+    seconds++; 
+    /* call the setTimeout( ) to keep the timer alive ! */ 
+    clearTime = setTimeout( "startTimer()", 1000 ); } 
+    // Function used to start the timer
+    function startTime() { 
+    /* check if seconds, minutes, and hours are equal to zero and start the timer*/ 
+    if ( seconds === 0 && minutes === 0 && hours === 0 ) {  
+    startTimer();
+    } }
+
+/* Function to stop timer */
+    function stopTimer( ) { 
+    /* check if seconds, minutes and hours are not equal to 0 */ 
+    if ( seconds !== 0 || minutes !== 0 || hours !== 0 ) { 
+    /* display the full time before reseting the stop watch */ 
+    var time = gethours + mins + secs;     
+    $('.score-section').find('.timer').html(time);
+    /*Add the time,moves and star rating to the congratulation modal only after game is complete*/
+   //  var StarsModalElem=$('.modal-element').eq(2);
+   //  var MovesModalElem=$('.modal-element').eq(1);
+   //  var TimerModalElem=$('.modal-element').eq(0);
+   //  $('.stars').clone().appendTo(StarsModalElem);
+   //  $('.moves').clone().appendTo(MovesModalElem);
+   //  $('.timer').clone().appendTo(TimerModalElem);
+    /* clear the stop watch using the setTimeout( ) return value 'clearTime' as ID */ 
+    clearTimeout( clearTime ); }}  
+    //Timer code end
 
 
+	// 	/****** Exit Message ******/
+	// 	$(window).bind('beforeunload', function(){
+	// 		return 'Are you sure you want to leave?';
+	// 	});
 
-	/* Flip the cards after 500 milliseconds */
-			// setTimeout(function(){
-			// 	/* Flip the card that haven't been matched */
-			// 	$('.card-start').toggleClass("card-end");
-			// },250);
+
