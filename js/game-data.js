e6a8df7eb59710a566cd67de9b4c737c0c6e680b
littleapple27 <38861849+littleapple27@users.noreply.github.com>
2020-03-16 10:51:32 -0400

    add ajax

--- js/game-data.js
+++ js/game-data.js
@@ -10,6 +10,19 @@ console.log(playername + timestamp);
 
 //Let's play button on-click 
 $(document).on('click', '#lets-play-btn', function () {
-	Cookies.set('player', playername + timestamp);
+	Cookies.set('playerId', playername + timestamp);
 });
 
+	API_URL='index.php';
+	$(document).on('submit','form#demoForm',function(e){
+		e.preventDefault();
+		$.post(API_URL,{
+			playerId:Cookies.get('playerId'),
+			age:$('form#demoForm').find('select[name="gender"]').val(),
+			ethnicity:$('form#demoForm').find('select[name="age"]').val(),
+			gender:$('form#demoForm').find('select[name="webUse"]').val(),
+			hours_pc:$('form#demoForm').find('select[name="education"]').val()
+		},function(data){
+			$('#demoModal').modal('hide');
+		});
+	});
