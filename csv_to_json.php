ebb44c2d43061ed1eb8884f8a9c09a91077b565d
littleapple27 <38861849+littleapple27@users.noreply.github.com>
2020-03-20 19:06:11 -0400

    Add files via upload

--- csv_to_json.php
+++ csv_to_json.php
@@ -5,17 +5,8 @@ $json=[];
 for($i=0;$i<count($csv)-1;$i++){
 	$csv[$i]=explode(';',$csv[$i]);
 	if(!isset($json[$csv[$i][2]])) $json[$csv[$i][2]]=[];
-	$json[$csv[$i][2]][]=$csv[$i]; // change 2 to whatever the index of the total score is.
+	$json[$csv[$i][2]][]=$csv[$i]; 
 }
 arsort($json);
 header('Content-Type: application/json');
 die(json_encode($json,JSON_PRETTY_PRINT));
-
-/* 
-if(!isset($json[$csv[$i][2]])) $json[$csv[$i][2]]=[];
-if(!isset($json[$csv[$i][2]][$csv[$i][3]])) $json[$csv[$i][2]][$csv[$i][3]]=[];
-if(!isset($json[$csv[$i][2]][$csv[$i][3]][$csv[$i][4]])) $json[$csv[$i][2]][$csv[$i][3]][$csv[$i][4]]=[];
-$json[$csv[$i][2]][$csv[$i][3]][$csv[$i][4]][]=$csv[$i];	// the element is an array nowif(!isset($json[$csv[$i][2]])) $json[$csv[$i][2]]=[];
-if(!isset($json[$csv[$i][2]][$csv[$i][3]])) $json[$csv[$i][2]][$csv[$i][3]]=[];
-if(!isset($json[$csv[$i][2]][$csv[$i][3]][$csv[$i][4]])) $json[$csv[$i][2]][$csv[$i][3]][$csv[$i][4]]=[];
-$json[$csv[$i][2]][$csv[$i][3]][$csv[$i][4]][]=$csv[$i]; */
